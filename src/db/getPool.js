8d33cdc1d24889df4d018c8a31b944fef6e6d5e2
JoseM-Alcazar <josemanuelalcazar12@gmail.com>
2024-09-25 11:16:47 +0200

    Estructura basica de carpetas y base de datos

---
+++ src/db/getPool.js
@@ -0,0 +1,36 @@
+//Accedemos a las variables de entorno personalizadas
+import 'dotenv/config';
+
+//Importamos la version asyn del modulo mysql2
+import mysql from 'mysql2/promise';
+
+//obtenemos las variables de entorno necesarias.
+const { MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB } = process.env;
+
+//variable que almacenara un grupo de conexiones con la db
+let pool;
+
+//funcion que retornara el pool 
+const getPool = async () => {
+
+    try {
+        //si no existe un "pool" de conexiones lo creamos
+        if (!pool) {
+            pool = mysql.createConnection({
+                host: MYSQL_HOST,
+                user: MYSQL_USER,
+                password: MYSQL_PASS,
+                //crear la base de datos y ponerla 
+                database: MYSQL_DB,
+                timezone: 'Z',
+            });
+        }
+        //retonro del pool
+        return await pool;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+//exportamos la funcion anterior
+export default getPool;

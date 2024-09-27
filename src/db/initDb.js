0fe3e81ae56086cec7e424994d50cac2fa428891
JoseM-Alcazar <josemanuelalcazar12@gmail.com>
2024-09-27 11:52:51 +0200

    InitDb actualizado

--- src/db/initDb.js
+++ src/db/initDb.js
@@ -1,7 +1,7 @@
-// Añadimos la configuraación del .env
+// Añadimos la configuración del .env
 import 'dotenv/config';
 
-// Importamos la funcion que  retona el pool de conexiones
+// Importamos la función que retorna el pool de conexiones
 import getPool from './getPool.js';
 
 // Generamos las tablas
@@ -11,10 +11,14 @@ const main = async () => {
 
         console.log('Borrando tablas');
 
-        await pool.query('DROP TABLE IF EXISTS doctorData, answers, consults, users');
+        // Eliminamos las tablas en el orden correcto
+        await pool.query(
+            'DROP TABLE IF EXISTS answers, consults, doctorData, specialities, users'
+        );
 
         console.log('Creando tablas');
 
+        // Creamos las tablas de manera individual
         await pool.query(`
             CREATE TABLE IF NOT EXISTS users (
                 id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
@@ -31,54 +35,105 @@ const main = async () => {
                 createdBy VARCHAR(20),
                 modifiedBy VARCHAR(20),
                 createdAt DATETIME DEFAULT CURRENT_TIMESTAMP, 
-                modifiedAt DATETIME ON UPDATE CURRENT_TIMESTAMP 
-            )
-            `);
+                modifiedAt DATETIME ON UPDATE CURRENT_TIMESTAMP
+            );
+        `);
+
         await pool.query(`
-            CREATE TABLE IF NOT EXISTS consults (
+            CREATE TABLE IF NOT EXISTS specialities (
                 id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-                userId INT UNSIGNED NOT NULL,
-                    FOREIGN KEY (userId) REFERENCES users(id),
-                title VARCHAR(50) NOT NULL,
-                
-                description VARCHAR(300) NOT NULL,
-                urgency ENUM('ALTA', 'MEDIA', 'BAJA') NOT NULL,
-                doctorId INT UNSIGNED NOT NULL,
-                    FOREIGN KEY (doctorId) REFERENCES users(id),
-                file VARCHAR(100),
-                createdAt DATETIME DEFAULT CURRENT_TIMESTAMP, 
-                modifiedAt DATETIME ON UPDATE CURRENT_TIMESTAMP 
-            )
-                `);
-        await pool.query(`
-            CREATE TABLE IF NOT EXISTS answers (
-                id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-                userId INT UNSIGNED NOT NULL,
-                    FOREIGN KEY (userId) REFERENCES users(id),
-                consultId INT UNSIGNED NOT NULL,
-                    FOREIGN KEY (consultId) REFERENCES consults(id),
-                answerText VARCHAR(1000) NOT NULL,
-                rating ENUM('1', '2', '3', '4', '5'),
-                file VARCHAR(100),
-                createdAt DATETIME DEFAULT CURRENT_TIMESTAMP, 
-                modifiedAt DATETIME ON UPDATE CURRENT_TIMESTAMP 
-                )
-                `);
+                name VARCHAR(20) NOT NULL,
+                createdAt DATETIME DEFAULT CURRENT_TIMESTAMP
+            );
+        `);
+
         await pool.query(`
             CREATE TABLE IF NOT EXISTS doctorData (
                 id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
                 userId INT UNSIGNED NOT NULL,
-                    FOREIGN KEY (userId) REFERENCES users(id),
-                specialty ENUM('Traumatologo', 'Oculista', 'Otorrino', 'Pediatra', 'Cardiologo', 'Urologo', 'Ginecologo', 'Odontologo', 'Neumologo', 'Dermatologo', 'Fisioterapeuta'),
+                FOREIGN KEY (userId) REFERENCES users(id),
+                specialityId INT UNSIGNED NOT NULL,
+                FOREIGN KEY (specialityId) REFERENCES specialities(id),
                 experience CHAR(2),
                 licenseNumber CHAR(9) NOT NULL,
                 createdBy VARCHAR(20),
                 modifiedBy VARCHAR(20),
                 createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-                modifiedAt DATETIME ON UPDATE CURRENT_TIMESTAMP 
-                )
-                `);
-        console.log('!Tablas creadas');
+                modifiedAt DATETIME ON UPDATE CURRENT_TIMESTAMP
+            );
+        `);
+
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS consults (
+                id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+                title VARCHAR(50) NOT NULL,
+                description VARCHAR(300) NOT NULL,
+                urgency ENUM('ALTA', 'MEDIA', 'BAJA') NOT NULL,
+                doctorId INT UNSIGNED,
+                FOREIGN KEY (doctorId) REFERENCES users(id),
+                file VARCHAR(100),
+                userId INT UNSIGNED NOT NULL,
+                FOREIGN KEY (userId) REFERENCES users(id),
+                specialityId INT UNSIGNED NOT NULL,
+                FOREIGN KEY (specialityId) REFERENCES specialities(id),
+                createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+                modifiedAt DATETIME ON UPDATE CURRENT_TIMESTAMP
+            );
+        `);
+
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS answers (
+                id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+                userId INT UNSIGNED NOT NULL,
+                FOREIGN KEY (userId) REFERENCES users(id),
+                consultId INT UNSIGNED NOT NULL,
+                FOREIGN KEY (consultId) REFERENCES consults(id),
+                answerText VARCHAR(1000) NOT NULL,
+                rating ENUM('1', '2', '3', '4', '5'),
+                file VARCHAR(100),
+                createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+                modifiedAt DATETIME ON UPDATE CURRENT_TIMESTAMP
+            );
+        `);
+
+        console.log('¡Tablas creadas!');
+
+        // Inserción de las especialidades en la tabla "specialities"
+        await pool.query(`
+            INSERT INTO specialities (name)
+            VALUES 
+                ('Traumatologo'),
+                ('Oculista'),
+                ('Otorrino'),
+                ('Pediatra'),
+                ('Cardiologo'),
+                ('Urologo'),
+                ('Ginecologo'),
+                ('Odontologo'),
+                ('Neumologo'),
+                ('Dermatologo'),
+                ('Fisioterapeuta');
+        `);
+
+        console.log('¡Datos insertados en specialities!');
+
+        // Inserción del usuario administrador
+        const adminUser = process.env.ADMINUSER;
+        const adminPass = process.env.ADMINPASS;
+        const adminMail = process.env.ADMINMAIL;
+
+        // Insertar el usuario administrador en la tabla "users"
+        await pool.query(
+            `
+            INSERT INTO users (username, password, firstName, lastName, email, role, active)
+            VALUES (?, ?, 'Admin', 'User', ?, 'admin', true)
+        `,
+            [adminUser, adminPass, adminMail]
+        );
+
+        console.log('¡Usuario administrador creado!');
+
+        // Cerrar la conexión
         process.exit(0);
     } catch (err) {
         console.error(err);
@@ -86,4 +141,5 @@ const main = async () => {
     }
 };
 
+// Llamamos a la función principal
 main();

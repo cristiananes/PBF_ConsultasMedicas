b2f26e6de74836e9832c7314f5edfaa3daf4df35
cristian <cristian-anes@hotmail.es>
2024-09-28 17:14:30 +0200

    encontrado un conflicto en el index de users y resuelto

--- src/controllers/users/index.js
+++ src/controllers/users/index.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-//crear el index de users
-=======
 // Importamos las funciones controladoras de las entradas.
 import adminRegisterController from './adminRegisterController.js';
 import userInfoController from './userInfoController.js';
@@ -17,4 +14,3 @@ export {
     userRegisterController,
     validateUserController,
 };
->>>>>>> 2032b9dba7ed59559cbe7cd6ce8f870332b4e5d6

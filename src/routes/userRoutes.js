c859b6af3b9e11eb2e61ed45231a6db63e8a40a2
JoseM-Alcazar <josemanuelalcazar12@gmail.com>
2024-10-02 13:11:40 +0200

    Añadido controlador para restringir el acceso solo a los administradores

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -9,80 +9,56 @@ import {
     validateUserController,
     listMedicsController,
     detallesMedicosController,
-
 } from '../controllers/users/index.js';
 
 //importamos los utils
 import {
+    authAdminController,
     authUserController,
-} from '../middlewares/index.js'
+} from '../middlewares/index.js';
 
 // Creamos el router a traves de express
 const router = express.Router();
 
-
-
 // Define tus rutas aquí
 
 //middleware que registra usuarios
-router.post(
-    `/user/register`,
-    userRegisterController,
-);
+router.post(`/user/register`, userRegisterController);
 
 //middleware que crea admis
 router.post(
     `/user/admin-register`,
-    authUserController,
-    adminRegisterController,
+    authAdminController,
+    adminRegisterController
 );
 
 //middleware que valida usuarios
-router.patch(
-    `/user/validate/:registrationCode`,
-    validateUserController,
-);
+router.patch(`/user/validate/:registrationCode`, validateUserController);
 
 //middleware de logini de user
-router.post(
-    `/user/login`,
-    userLoginController,
-);
+router.post(`/user/login`, userLoginController);
 
 //middleware que retorna info del user
-router.get(
-    `/user/:userId`,
-    authUserController,
-    userInfoController,
-
-);
+router.get(`/user/:userId`, authUserController, userInfoController);
 
 //middleware que cambia la contraseña
-router.patch(
-    `/user/change-password`,
-    authUserController,
-);
+router.patch(`/user/change-password`, authUserController);
 
 //middleware que retorna el listado de medicos
 router.get(
     `/users/doctors`,
     //añadir cuando se cree
     authUserController,
-    listMedicsController,
+    listMedicsController
 );
 
 //middleware que retorna las especialidades
 router.get(
     `/user/specialties`,
     //añadir cuando se cree clase
-    authUserController,
-
+    authUserController
 );
 
 //middleware detalle medico
-router.get(
-    `/doctor/:doctorId`,
-    authUserController,
-    detallesMedicosController,
-);
+router.get(`/doctor/:doctorId`, authUserController, detallesMedicosController);
 export default router; // Esta es la exportación por defecto

9636afe3474c1478e7ad263b1421a67a8d2169ad
JoseM-Alcazar <josemanuelalcazar12@gmail.com>
2024-10-02 13:11:40 +0200

    Añadido controlador para restringir el acceso solo a los administradores

---
+++ src/middlewares/authAdminController.js
@@ -0,0 +1,28 @@
+// Importamos dependencias
+import jwt from 'jsonwebtoken';
+
+// Importamos el util de generar error
+import generateErrorUtil from '../utils/generateErrorUtil.js';
+
+const authUserController = async (req, res, next) => {
+    try {
+        // Comprobamos si existe autorización y generamos error en caso contrario.
+        const { authorization } = req.headers;
+        if (!authorization) {
+            generateErrorUtil('Falta la cabecera de autorización', 401);
+        }
+        // Si existe autorización, creamos el token de usuario
+        try {
+            const tokenInfo = jwt.verify(authorization, process.env.SECRET);
+            req.user = tokenInfo;
+            next();
+        } catch (err) {
+            console.error(err);
+            generateErrorUtil('Token invalido', 401);
+        }
+    } catch (err) {
+        next(err);
+    }
+};
+
+export default authUserController;
